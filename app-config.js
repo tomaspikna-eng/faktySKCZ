d9f5d0e9d5e724305aaef77731fe45eec1a8a98d
Tomino Project <tomaspikna@gmail.com>
2026-09-25 18:10:29 +0200

    Add public archive API config

---
+++ app-config.js
@@ -0,0 +1,4 @@
+window.FAKTY_CONFIG = Object.freeze({
+  archiveApi: "https://mexrrchqiehzvrefftym.supabase.co/functions/v1/archive-api",
+  publishableKey: "sb_publishable_NZCEN4vfkbyxQrzCa8YR8Q_i4ZQCH2T"
+});
