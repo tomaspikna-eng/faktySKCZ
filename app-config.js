a6a2dd7169883c15f68515a92cc3bfae63326361
Tomino Project <tomaspikna@gmail.com>
2026-09-26 10:51:38 +0200

    Set current DETEKTOR extension version to v0.7.3

--- app-config.js
+++ app-config.js
@@ -3,5 +3,5 @@ window.FAKTY_CONFIG = Object.freeze({
   archiveApi: "https://mexrrchqiehzvrefftym.supabase.co/functions/v1/archive-api",
   publishableKey: "sb_publishable_NZCEN4vfkbyxQrzCa8YR8Q_i4ZQCH2T",
   downloadUrl: "https://mexrrchqiehzvrefftym.supabase.co/storage/v1/object/public/downloads/DETEKTOR_Streamer_latest.zip",
-  extensionVersion: "0.7.1"
+  extensionVersion: "0.7.3"
 });
